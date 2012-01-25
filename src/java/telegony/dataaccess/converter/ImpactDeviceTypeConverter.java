84d8edb69755349c8da21d68bc03aa62ff63f075
fedul0x <fedul0x@sheldor>
2012-01-25 19:44:37 +0300

    Исправлены конвертеры SensorDevice и ImpactDevice

--- src/java/telegony/dataaccess/converter/ImpactDeviceTypeConverter.java
+++ src/java/telegony/dataaccess/converter/ImpactDeviceTypeConverter.java
@@ -3,24 +3,24 @@ package telegony.dataaccess.converter;
 import org.apache.click.util.RequestTypeConverter;
 import org.apache.velocity.exception.ParseErrorException;
 import telegony.dataaccess.RepositoryProvider;
-import telegony.hardware.SensorDevice;
+import telegony.hardware.ImpactDevice;
 
 /**
- * Конвертер запроса для сущности SensorDevice
+ * Конвертер запроса для сущности ImpactDevice
  * @author Ivashin Alexey
  */
-public class SensorDeviceTypeConverter extends RequestTypeConverter {
+public class ImpactDeviceTypeConverter extends RequestTypeConverter {
 
     @Override
     protected Object convertValue(Object value, Class<?> toType) {
-        if (toType.equals(SensorDevice.class)) {
+        if (toType.equals(ImpactDevice.class)) {
             Long id;
-             try {
-                id = Long.valueOf((String)value);
+            try {
+                id = Long.valueOf((String) value);
             } catch (NumberFormatException ex) {
                 throw new ParseErrorException("Ошибка в запросе для сущности " + toType.toString());
             }
-            return (SensorDevice) RepositoryProvider.getRepository(SensorDevice.class).findById(id);
+            return (ImpactDevice) RepositoryProvider.getRepository(ImpactDevice.class).findById(id);
         }
         return super.convertValue(value, toType);
     }
