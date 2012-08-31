93edb5fd244307ff3faac42f16f654699230e490
fedul0x <fedul0x@sheldor>
2012-08-31 10:42:10 +0400

    Добавлены сущности для хранения показаний датчиков и значений управляющих воздействий

---
+++ src/java/telegony/hardware/statement/ImpactDeviceValue.java
@@ -0,0 +1,14 @@
+package telegony.hardware.statement;
+
+import telegony.general.TransientObject;
+import telegony.hardware.ImpactDevice;
+
+/**
+ * Сущность хранений упраляющего воздействия
+ * @author Ivashin Alexey
+ */
+public class ImpactDeviceValue extends TransientObject {
+    private ImpactDevice impactDevice;
+    
+    
+}
---
+++ src/java/telegony/hardware/statement/SensorDeviceValue.java
@@ -0,0 +1,15 @@
+package telegony.hardware.statement;
+
+import telegony.general.TransientObject;
+import telegony.hardware.SensorDevice;
+
+/**
+ * Сущность хранения показаний датчика
+ * @author Ivashin Alexey
+ */
+public class SensorDeviceValue extends TransientObject{
+    private SensorDevice sensorDevice;
+    
+    
+    
+}
